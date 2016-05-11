15a09aeabc16b4e754eb296b675eba57ffd8bf6d
Jarno Kiviaho <jarkki@kapsi.fi>
2016-05-11 09:48:31 +0300

    Tweaks

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Reaktor Orbital Challenge solution
+![](plot.png?raw=true)
+
+This Python script solves the [Reaktor Orbital Challenge](https://reaktor.com/orbital-challenge/) by using the [Dijkstra shortest path algorithm](https://en.wikipedia.org/wiki/Dijkstra's_algorithm) to find the shortest path from caller to recipient through the given satellite configuration.
+
+## Author
+Jarno Kiviaho <jarkki@kapsi.fi>
+Github: https://github.com/jarkki
+
+## Dependencies
+    * Python version >= 2.7
+    Optional:
+    * Matplotlib for plotting
+    
+## Run
+Execute
+```bash
+    python route.py
+```
+from command line.
--- route.py
+++ route.py
@@ -1,8 +1,12 @@
-import pdb
+#
+#  Solution to the Reaktor Orbital Challenge
+#    https://reaktor.com/orbital-challenge/
+#
+#  Copyright (C) 2016  Jarno Kiviaho <jarkki@kapsi.fi>
+#
+#
+import sys
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import urllib.request
 
 def radians(degrees):
     return degrees * np.pi / 180.0
@@ -19,10 +23,11 @@ def latlon_to_cartesian(radius, lat, lon):
     z = radius * np.sin(radians(lat))
     return (x,y,z)
 
-def plot_earth(ax, radius = 6371.0):
+def plot_earth(ax, radius = 6371.0, col='#fff1d0'):
+    """ Plot earth as a perfect sphere """
     theta, phi = np.mgrid[0:2*np.pi:40j, 0:np.pi:20j]
     x,y,z = spherical_to_cartesian(radius, theta, phi)
-    return ax.plot_surface(x, y, z, alpha=0.05, color='#fff1d0', rstride=1, cstride=1, linewidth=1, shade=1)
+    return ax.plot_surface(x, y, z, alpha=0.05, color=col, rstride=1, cstride=1, linewidth=1, shade=1)
 
 def plot_point(x,y,z, ax, col='#086788', size=50):
     return ax.scatter([x], [y], [z], c=col, s=size, edgecolor='none')
@@ -77,27 +82,27 @@ def line_sphere_intersect(radius, xyz1, xyz2, xyz3=(0.0,0.0,0.0)):
         return []
 
 def distance(xyz1, xyz2):
-    """ Distance between two points"""
+    """ Distance between two points in 3D-space"""
     x1,y1,z1 = xyz1
     x2,y2,z2 = xyz2
     return np.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
 def equal(xyz1, xyz2, tol=1e-8):
-    """ Checks if two points in 3D-space have the same location, with a tolerance"""
-    x1,y1,z1 = xyz1
-    x2,y2,z2 = xyz2
-
-    if abs(x1-x2) < tol and abs(y1-y2) < tol and abs(z1-z2) < tol:
+    """ Checks if two points in 3D-space have the same location, within tolerance."""
+    if distance (xyz1, xyz2) < tol:
         return True
     else:
         return False
 
 def satellite_graph(coord, earth_radius):
-    """ Create graph for satellites that have un-obstructed view to each other and call locations.
+    """ Create graph for satellites and call locations.
+
+    Two satellites are connected if there is an un-obstructed view from one to another. Edge weights are the distances between satellites. Call locations are included in the graph.
 
     Args:
     -----
-    coord     -- A dict of satellite coordinates (Cartesian) including call locations with names 'Source and 'Target'.
+    coord        -- A dict of satellite coordinates (Cartesian) including call locations with names 'Source and 'Target'.
+    earth_radius -- The radius of earth.
 
     Returns:
     --------
@@ -112,29 +117,45 @@ def satellite_graph(coord, earth_radius):
 
     for key1 in coord.keys():
         for key2 in coord.keys():
-            if key1 != key2:
+            if key1 not in ['Source', 'Target', key2]:
+                # Check if the line created by the two points (satellites/call locations) intersects the planet sphere
                 intersect = line_sphere_intersect(earth_radius, coord[key1], coord[key2])
                 if key2 == 'Source' or key2 == 'Target':
                     if len(intersect) >= 0:
+                        # Two intersections or one tangent.
+                        # Check if the intersection closest to key1 is the call location
+                        # If so, the satellite has un-obstructed view to the call location.
                         if equal(intersect[0], coord[key2]):
+                            # Un-obstructed view
                             dist = distance(coord[key1], coord[key2])
                             graph[key1][key2] = dist
                             graph[key2][key1] = dist
                 else:
                     if len(intersect) == 0:
+                        # No intersection, two satellites have un-obstructed view
                         graph[key1][key2] = distance(coord[key1], coord[key2])
 
-    # Remove nodes with no vertices
+    # Remove nodes with no connections
     graph = dict((k, v) for k, v in graph.items() if v)
 
     if not 'Source' in graph.keys() or not 'Target' in graph.keys():
-        print (graph)
         raise ValueError("There is no path from source to target!")
 
     return graph
 
 def min_dist_vertex(Q, dist):
+    """ Search for the vertex with the minimum distance from source, in vertices given in Q.
 
+    Args:
+    -----
+    Q     --  List of vertice names.
+    dist  --  Dict, representing the distance from source to vertex v.
+
+    Returns:
+    --------
+    The name of the vertex with the minimum distance from source.
+
+    """
     min_vertex = Q[0]
 
     for vertex in Q[1:len(Q)]:
@@ -144,17 +165,17 @@ def min_dist_vertex(Q, dist):
     return min_vertex
 
 def dijkstra(graph, source, target):
-    """ The Dijkstra algorithm for finding the shortest path between two nodes in an undirected weighted graph
+    """ The Dijkstra algorithm for finding the shortest path between two vertices in an undirected weighted graph.
 
     Args:
     -----
-    graph       --  A dict of dicts representation of the graph with weights (distances).
-    source      --  Name of the starting node
-    target --  Name of the destination node
+    graph    --  A dict of dicts representation of the graph with weights (distances).
+    source   --  Name of the starting vertex
+    target   --  Name of the destination vertex
 
     Returns:
     --------
-    A list with node names ordered to give the shortest path from source to target
+    An ordered list with node names to give the shortest path from source to target
     """
     dist = dict() # Distance from source to vertex v
     prev = dict() # Previous node in the optimal path from source
@@ -190,14 +211,33 @@ def dijkstra(graph, source, target):
     return path
 
 def plot_solution(graph, coord, path, col):
+    """ Use matplotlib to plot the solution
+
+    Note: the plot blocks the script execution until the it is closed.
+
+    Args:
+    -----
+    graph  --  Dict of dicts graph representation
+    coord  --  Coordinates for each satellite and call location
+    path   --  The shortest path from call source to target
+    col    --  Dict of colors for plotting
+    """
+
+    # Check if matplotlib is installed
+    try:
+        import matplotlib
+        import matplotlib.pyplot as plt
+        from mpl_toolkits.mplot3d import Axes3D
+    except ImportError as e:
+        print(e)
+        print("Matplotlib not installed, can't plot.")
+        return
 
     # Init plotting
-    plt.ion()
     fig = plt.figure()
     ax = fig.gca(projection='3d')
     ax.set_aspect("equal")
     ax.set_axis_bgcolor(col['bg'])
-
 
     # Plot earth
     plot_earth(ax)
@@ -224,14 +264,29 @@ def plot_solution(graph, coord, path, col):
     plt.axis('off')
     plt.show()
 
-def dl_data(filename='data.csv'):
+def dl_data(url='http://space-fast-track.herokuapp.com/generate',
+            filename='data.csv'):
     """ Downloads the problem data and writes to csv file.
 
     Does not handle exceptions.
 
+    Args:
+    -----
+    url       --  The url for the data
+    filename  --  Name of the csv file to write to.
+
     """
-    response = urllib.request.urlopen('http://space-fast-track.herokuapp.com/generate')
-    data = response.read().decode('utf-8')
+    # Handle different urllib implementation for python 2 and 3
+    py3 = sys.version_info[0] == 3
+    if py3:
+        import urllib.request
+        response = urllib.request.urlopen(url)
+        data = response.read().decode('utf-8')
+    else:
+        import urllib
+        response = urllib.urlopen(url)
+        data = response.read()
+
     file = open(filename, 'w')
     file.write(data)
     file.close()
@@ -252,7 +307,7 @@ def read_data(filename='data.csv'):
     """
     # Open file and read the data
     file = open(filename, 'r')
-    data = file.read() 
+    data = file.read()
     file.close()
 
     # Split the rows
@@ -289,7 +344,7 @@ def solve_challenge(dl_new_data=False):
     if dl_new_data:
         dl_data()    # Download new dataset and save to 'data.csv'
 
-    # Read in data from file
+    # Read in data from 'data.csv'
     data = read_data()
 
     # Transform from lat and long to Cartesian coordinates
@@ -297,17 +352,17 @@ def solve_challenge(dl_new_data=False):
     for sat in data['sats']:
         coord[sat['ID']] = latlon_to_cartesian(earth_radius + sat['alt'], sat['lat'], sat['lon'])
 
-    # Add call locations
+    # Add source and target call locations
     coord['Source'] = latlon_to_cartesian(earth_radius, data['call_loc']['lat1'], data['call_loc']['lon1'])
     coord['Target'] = latlon_to_cartesian(earth_radius, data['call_loc']['lat2'], data['call_loc']['lon2'])
 
-    # Create the graph (connections between satellites and locations that have clear view to each other) with distances as edges
+    # Create the graph (connections between satellites (and locations) that have clear view to each other) with distances as edges
     graph = satellite_graph(coord, earth_radius)
 
     # Calculate the shortest path from source to target
     shortest_path = dijkstra(graph, 'Source', 'Target')
 
-    # Print the path
+    # Print the path and seed
     path_str = "{}".format(shortest_path[1:len(shortest_path)-1]).replace("'","")
     print("Shortest path: {}, seed: {}".format(path_str, data['seed']))
 
